549cd4f8372c557b85de83fe9158c1d5ee56580f
unknown <yanghuning@imeik.net>
2021-11-06 00:33:17 +0800

    '开发环境配置终端信息输出封装'

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,13 +1,11 @@
 'use strict'
 process.env.TAG = 'dev';
 
-let os = require('os');
 let path = require('path');
-let chalk = require('chalk');
 let portfinder = require('portfinder');
 let { merge } = require('webpack-merge');
+let handleOutMessage = require('./utils/clientOut');
 let webpackBaseConfig = require('./webpack.base');
-let FriendlyErrorsPlugin = require('@soda/friendly-errors-webpack-plugin');
 let ESLintPlugin = require('eslint-webpack-plugin');
 let config = require('./')[process.env.TAG];
 
@@ -61,55 +59,15 @@ module.exports = new Promise((resolve, reject) => {
         if (err) {
             reject(err);
         } else {
+            
             // 将新端口设置到全局，以便后期测试用例使用
             process.env.PORT = port;
 
             // 将新的端口号覆盖到配置中
             devConfig.devServer.port = port;
 
-            // 查找一下局域网IP地址
-            let curNetIp4 = '';
-            let curNetIp6 = '';
-            let netIpList = os.networkInterfaces().WLAN;
-            
-            if (netIpList) {                
-                netIpList.map(item => {
-                    if (item.family === 'IPv4') {
-                        curNetIp4 = item.address;
-                    } else if (item.family === 'IPv6') {
-                        curNetIp6 = item.address;
-                    }
-                });
-            }
-
-            let TipsList = '';
-
-            if (devConfig.devServer.host === '0.0.0.0') devConfig.devServer.host = undefined;
-
-            if (devConfig.devServer.host) {
-                TipsList = `- 本地 ${ chalk.cyan('http://' + devConfig.devServer.host + ':' + chalk.cyanBright(port)) }`;
-            } else if (devConfig.devServer.host === undefined) {
-                TipsList =  `- 本地IPv4 ${ chalk.cyan('http://localhost:' + chalk.cyanBright(port)) }
-- 网络IPv4 ${ curNetIp4 ? chalk.cyan('http://' + curNetIp4 + ':' + chalk.cyanBright(port)) : chalk.gray('unavailable') }
-- 网络IPv6 ${ curNetIp6 ? chalk.cyan('http://['+ curNetIp6 + ']:' + chalk.cyanBright(port)) : chalk.gray('unavailable') }`
-            }
-
-            // 启动完成友好提示
-            devConfig.plugins.push(new FriendlyErrorsPlugin({
-                compilationSuccessInfo: {
-                    messages: [`你的服务已经启动，请访问: 
-
-${TipsList}
-
-注意：这是开发环境未优化的编译模式
-如果想编译测试版本，请运行 ${ chalk.cyanBright('npm run stage') }
-如果想编译生产版本，请运行 ${ chalk.cyanBright('npm run build') }
-                    `],
-                }
-            }))
-
-            // 返回配置
-            resolve(devConfig);
+            // 处理并返回开发服务配置
+            resolve(handleOutMessage(devConfig));
         }
     })
 })
